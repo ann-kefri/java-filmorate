dd566dd117e5b6e9f43f4e39ad3cc7eca52d6f5f
annkefri <annkefri@yandex.ru>
2026-08-27 12:40:52 +0300

    Исправить стиль кода.

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -12,4 +12,5 @@ public class User {
     private String login;
     private String name;
     private LocalDate birthday;
-    private Set<Integer> friends = new HashSet<>();}
+    private Set<Integer> friends = new HashSet<>();
+}
