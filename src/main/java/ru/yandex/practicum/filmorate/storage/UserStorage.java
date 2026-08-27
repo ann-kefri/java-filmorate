c69293c99b5f63ed4dd18e3135cb31618ab40f49
annkefri <annkefri@yandex.ru>
2026-08-27 12:46:56 +0300

    Исправить стиль кода.

--- src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/UserStorage.java
@@ -1,18 +1,28 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import ru.yandex.practicum.filmorate.model.User;
+
 import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
     User create(User user);
+
     User update(User user);
+
     List<User> getAll();
+
     Optional<User> getById(int id);
+
     void delete(int id);
+
     List<User> getFriends(int userId);
+
     void addFriend(int userId, int friendId);
+
     void confirmFriend(int userId, int friendId);
+
     void removeFriend(int userId, int friendId);
+
     List<User> getCommonFriends(int userId, int otherId);
 }
